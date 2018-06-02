dd677f1c05ea304e2ee40af96e03e3745805d6bd
Dnguy104 <dnguy104@ucr.edu>
2018-06-01 23:46:39 -0700

    finishing button even listener function

---
+++ script.js
@@ -0,0 +1,37 @@
+var boardGame = (() => {
+	var gameArray = [];
+	var clear = () => {
+		for(let i = 0; i < 9; i++) {
+			gameArray.push('');
+		}	
+	};
+	var render = () => {
+		var gameBoard = document.getElementById('gameBoard');
+		for(let i = 0; i < 9; i++) {
+			var block = document.createElement('div');
+			block.style.backgroundColor = '#d6d6d6';
+			gameBoard.appendChild(block);
+		}
+	};
+	var markBoard = (position, player) => {
+		gameArray[position] = player;
+		render();
+	};
+	var initButtons = (player) => {
+		var gameBoard = document.getElementById('gameBoard');
+		var children = gameBoard.childNodes;
+		children.forEach((x) => {
+			x.addEventListener('click', (event) => {
+				if(gameArray(event.target.id) == '') {
+					this.markBoard(event.target.id, player);
+				}	
+			})
+		});
+	};
+	
+	
+	
+	render();	
+	return {render}
+})();
+
